eb835c7ea60b067fb92b3eeb3cb8c08ab8980dd0
michelguilhen <guilhenmichel@gmail.com>
2024-03-24 19:36:17 -0600

    config + cors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
 
 import cityRoutes from "./routes/city.routes";
 import weatherRoutes from "./routes/weather.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", cityRoutes);
 app.use("/api", weatherRoutes);
 
 app.listen(3000, () => {
-  console.log("Application started and available on port 3000");
-  console.log("ENV: ", process.env.NODE_ENV);
+  console.log("Application listing on port", process.env.PORT);
+  console.log("Environment:", process.env.NODE_ENV);
 });
